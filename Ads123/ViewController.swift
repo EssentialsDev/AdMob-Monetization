12b0281773ef4d25dcad0e42d5429a9496ed8c64
Kasey Schlaudt <kaseyschlaudt@gmail.com>
2017-01-17 20:44:19 -0700

    finished

--- Ads123/ViewController.swift
+++ Ads123/ViewController.swift
@@ -7,19 +7,18 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var adView: GADBannerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        adView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        adView.rootViewController = self
+        adView.load(GADRequest())
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
